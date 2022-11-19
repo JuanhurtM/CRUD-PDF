447b93e7b3422d29d602f02e7c23ae301ea53aa7
JuanhurtM <jjuanjose1019@gmail.com>
2022-11-18 23:07:19 -0500

    Cambios a la interfaz

--- controllers/eliminar.php
+++ controllers/eliminar.php
@@ -1,15 +1,13 @@
 <?php 
 
 $id=$_GET['id'];
-include '../models/archivo.php';
 include '../config/bd.php';
+include '../models/archivo.php';
 $conexion=conexion();
 $query=eliminar($conexion,$id);
-
 if($query){
- header('location:../index.php?eliminar=success');
-}else{
-    header('location:../index.php?eliminar=error');
-}
-
+    header('location:../index.php?eliminar=success');
+   }else{
+       header('location:../index.php?eliminar=error');
+   }
 ?>
--- index.php
+++ index.php
@@ -70,9 +70,12 @@
                     <td><?php echo $contador;?></td>
                     <td><?php echo $nombre ;?></td>
                     <td><?php echo $categoria;?></td>
-                    <td><a href="cargar.php?id=<?php echo $id; ?>"><?php echo $valor ;?> descargar</a></td>
+                    <td><a class='btn btn-success' href="cargar.php?id=<?php echo $id; ?>"><?php echo $valor ;?> descargar</a></td>
                     <td><?php echo $fecha ;?></td>
-                    <td><a class='btn btn-secondary' href="editar.php?id=<?php echo $id?>">Editar</a> <a class='btn btn-danger' href="controllers/eliminar.php?id=<?php echo $id?>">Eliminar</a></td>
+                    <td>
+                        <a class='btn btn-secondary' href="editar.php?id=<?php echo $id?>">Editar</a>
+                        <a class='btn btn-danger' href="controllers/eliminar.php?id=<?php echo $id?>">Eliminar</a>
+                    </td>
                 </tr>
                 <?php 
                     }
@@ -82,6 +85,6 @@
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
